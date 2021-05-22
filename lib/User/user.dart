7b7b41b1c38b49028050c08c34c7726bc906a580
Tushar Kalbhande <tusharkalbhande1805@gmail.com>
2021-05-22 18:44:44 +0530

    New Files

---
+++ lib/User/user.dart
@@ -0,0 +1,22 @@
+import 'package:way_to_heaven/utils/utils.dart';
+
+class User {
+  String name, role, address;
+  DateTime createdTime;
+
+  User({this.name, this.createdTime, this.role, this.address});
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'createdTime': createdTime,
+        'role': role,
+        'address': address
+      };
+
+  static User fromJson(Map<String, dynamic> json) => User(
+        createdTime: Utils.toDateTime(json['createdTime']),
+        name: json['name'],
+        role: json['role'],
+        address: json['address'],
+      );
+}
